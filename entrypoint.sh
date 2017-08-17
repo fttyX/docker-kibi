fef3104f632b737397f6123b676484d9a2538b78
feature <fttyx@icloud.com>
2017-08-17 10:40:31 +0300

    Modified entrypoint script to process ELASTICSEARCH_URL argument correctly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,14 @@
 
 set -e
 
-if [ "$1" = 'docker-kibi' ]; then
-    if [ "$ELASTICSEARCH_URL" -o "$ELASTICSEARCH_PORT_9200_TCP" ]; then
-        : ${ELASTICSEARCH_URL:='http://elasticsearch:9200'}
-        sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibi/config/kibi.yml
-    else
-        "No ES URL parameter, starting local instance... "
-        service elasticsearch start
-        sleep 5
-    fi
-else
-        service elasticsearch start
-        sleep 5
+if [ -z ${ELASTICSEARCH_URL} ]; then
+  ELASTICSEARCH_URL="localhost"
+  service elasticsearch start
 fi
 
 # Patch demo kibi to use standard ES port
 perl -p -i -e "s/9220/9200/" /opt/kibi/config/kibi.yml
-perl -p -i -e "s/localhost/0.0.0.0/" /opt/kibi/config/kibi.yml
+perl -p -i -e "s/localhost/$ELASTICSEARCH_URL/" /opt/kibi/config/kibi.yml
 
 # Start Kibi
 /opt/kibi/bin/kibi &
