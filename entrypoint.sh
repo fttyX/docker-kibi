714897b1b96bca9b688873abd00d96d425d3e554
feature <fttyx@icloud.com>
2017-08-17 11:07:51 +0300

    Fixed elasticsearch tail bug.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 if [ -z ${ELASTICSEARCH_URL} ]; then
   ELASTICSEARCH_URL="localhost"
   service elasticsearch start
+  tail -f /var/log/elasticsearch/*.log
 fi
 
 # Patch demo kibi to use standard ES port
@@ -13,4 +14,3 @@ perl -p -i -e "s/localhost/$ELASTICSEARCH_URL/" /opt/kibi/config/kibi.yml
 
 # Start Kibi
 /opt/kibi/bin/kibi &
-tail -f /var/log/elasticsearch/*.log
