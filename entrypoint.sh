621aed26cd356d1b943bc15700062a3453741c88
feature <fttyx@icloud.com>
2017-08-17 12:19:31 +0300

    Set elasticsearch.url kibi option.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,11 @@ if [ -z ${ELASTICSEARCH_URL} ]; then
 #  tail -f /var/log/elasticsearch/*.log
 fi
 
+sed -ri "s/^(\#\s*)?(elasticsearch\.url:).*/\2 '$ELASTICSEARCH_URL'/" /opt/kibi/config/kibi.yml
+
 # Patch demo kibi to use standard ES port
 perl -p -i -e "s/9220/9200/" /opt/kibi/config/kibi.yml
-perl -p -i -e "s/localhost/$ELASTICSEARCH_URL/" /opt/kibi/config/kibi.yml
+perl -p -i -e "s/localhost/0.0.0.0/" /opt/kibi/config/kibi.yml
 
 # Start Kibi
 /opt/kibi/bin/kibi &
